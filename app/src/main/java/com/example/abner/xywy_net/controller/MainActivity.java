ef00fe71153487e56ebb0ff3c2ef4433f88c1e00
1450030827@qq.com <qwert12345++>
2017-06-09 17:02:39 +0800

    //忽略文件

--- app/src/main/java/com/example/abner/xywy_net/controller/MainActivity.java
+++ app/src/main/java/com/example/abner/xywy_net/controller/MainActivity.java
@@ -1,17 +1,54 @@
 package com.example.abner.xywy_net.controller;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
 
 import com.example.abner.xywy_net.R;
+import com.example.abner.xywy_net.base.BaseActivity;
 import com.example.abner.xywy_net.utils.netutils.ForNet;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
+    private FrameLayout frameLayout;
+    private RadioButton button1,button2,button3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //ForNet.getNetState()
+    protected int layoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        frameLayout= (FrameLayout) findViewById(R.id.MainActivity_FrameLayout);
+        button1= (RadioButton) findViewById(R.id.MainActivity_Button1);
+        button2= (RadioButton) findViewById(R.id.MainActivity_Button2);
+        button3= (RadioButton) findViewById(R.id.MainActivity_Button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
--- app/src/main/java/com/example/abner/xywy_net/controller/activity/SplashActivity.java
+++ app/src/main/java/com/example/abner/xywy_net/controller/activity/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.example.abner.xywy_net.controller.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.widget.TextView;
 
 import com.example.abner.xywy_net.R;
@@ -12,6 +13,8 @@ import com.example.abner.xywy_net.controller.MainActivity;
  */
 
 public class SplashActivity extends BaseActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 3000; //延迟三秒
     @Override
     protected int layoutId() {
         return R.layout.activity_splash;
@@ -19,10 +22,18 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     protected void initView()  {
+        new Handler().postDelayed(new Runnable(){
+
+            public void run() {
+            Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+            SplashActivity.this.startActivity(mainIntent);
+            SplashActivity.this.finish();
+        }
+
+    }, SPLASH_DISPLAY_LENGHT);
 
 
-
-    }
+}
 
     @Override
     protected void initData() {
